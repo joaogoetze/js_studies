2f7d50c14f021d095ae8b386371013b683fffce7
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-11-11 21:34:32 -0300

    adding basic js uses

---
+++ function.js
@@ -0,0 +1,5 @@
+function somaValor(n1, n2){
+    return(n1 + n2);
+}
+
+console.log(somaValor(10, 20));
---
+++ if.js
@@ -0,0 +1,5 @@
+if(1>2){
+    console.log("1 maior que 2");
+} else {
+    console.log("2 maior que 1");
+}

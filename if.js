073adba3a6845a205f791f11ead0f75bf98e4a9f
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-11-15 18:16:49 -0300

    if types

--- if.js
+++ if.js
@@ -1,5 +1,36 @@
+//IF ELSE normal
+
 if(1>2){
     console.log("1 maior que 2");
 } else {
     console.log("2 maior que 1");
 }
+
+//TRUE OU FALSE
+
+const idade = 18;
+
+const resultadoComTexto = idade >= 18 ? "Maior de idade" : "Menor de idade";
+console.log(resultadoComTexto);
+
+const resultadoComBoolean = idade >= 18 ? true : false;
+console.log(resultadoComBoolean);
+
+//TRUE ou FALSE COM &&
+
+const resultadoE = 1 > 0 && 1<2 ? true : false; // Se as duas condições forem verdadeiras, o resultado é true
+console.log(resultadoE);
+
+
+//CONDIÇÃO &&
+
+idade >= 18 && console.log("Maior de idade!"); // Se a condição for verdadeira, executa
+
+
+//CONDIÇÃO ||
+
+const resultadoOU = idade || "Sem idade";
+console.log(resultadoOU);
+
+// Se IDADE tiver um valor FALSE ou NULL, será printado "Sem idade"
+// Mas se IDADE tiver algum valor, será printada a idade

2dd9292d1ebd43a0317b04047fc6178d5902bd55
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-11-15 21:48:01 -0300

    functions

--- function.js
+++ function.js
@@ -1,5 +1,26 @@
+
+// Função normal
+
 function somaValor(n1, n2){
     return(n1 + n2);
 }
 
 console.log(somaValor(10, 20));
+
+// Arrow function
+
+//Com apenas uma expressão
+const somaValores = (n1, n2) =>  n1 + n2; //Como só existe uma expressão, não é necessário return
+
+console.log(somaValores(1, 2));
+
+//Com apenas um parâmetro
+
+const somaValorUmParametro = x => x * 2;
+
+console.log(somaValorUmParametro(3));
+
+// Sem nenhum parâmetro
+const escreverMensagem = () => console.log("Mensagem");
+
+escreverMensagem();
--- if.js
+++ if.js
@@ -34,3 +34,21 @@ console.log(resultadoOU);
 
 // Se IDADE tiver um valor FALSE ou NULL, será printado "Sem idade"
 // Mas se IDADE tiver algum valor, será printada a idade
+
+const idadeSwitch = 25;
+
+//Switch case
+
+switch (idadeSwitch){
+    case 17:
+        console.log("17 anos");
+        break;
+    case 18:
+        console.log("18 anos");
+        break;
+    case 19:
+        console.log("19 anos");
+        break;
+    default:
+        console.log("Outra idade");
+}

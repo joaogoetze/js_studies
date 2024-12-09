d9d25b56b32e2e2d4b87b21b3d7c92ce0605def4
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-12-09 20:43:24 -0300

    updating array file

--- arrays.js
+++ arrays.js
@@ -1,12 +1,17 @@
+// Array de apenas um tipo
 const list = ["51980276791", "51998913436"];
 
-//Printar lista
+// Printar lista
 console.log(list);
 
-//Printar elemento específico da lista
+// Printar elemento específico da lista
 console.log(list[1]);
 
-//Percorre toda a lista e printa cada valor
+// Percorre toda a lista e printa cada valor
 for(x=0; x<list.length; x++){
     console.log(list[x]);
 }
+
+// Array de vários tipos
+const array = ["João", 21, 1.75];
+console.log(array);
